5a808ba6ee563ecc6a7772cbdd8a8807d09764e7
Romain Morvant <romainmorvantdev@gmail.com>
2022-02-05 14:10:51 +0100

    Récupération du profil de l'utilisateur connecté

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @route       GET api/profile
-// @desc        Route test
-// @access      Public
-router.get('/', (req, res) => res.send('Route profil'));
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route       GET api/profile/me
+// @desc        Récupération du profile de l'utilisateur en cours
+// @access      Private
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user',
+            ['name', 'avatar']);
+
+        if (!profile) {
+            return res.status(400).json({ msg: "Cet utilisateur n'a pas de profil .." });
+        }
+
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Erreur serveur ..');
+    }
+});
 
 module.exports = router;
