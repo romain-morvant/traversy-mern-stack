af397fa0ae0d6d2bbac370ca06317662c529484e
Romain Morvant <romainmorvantdev@gmail.com>
2022-02-05 14:57:13 +0100

    Ajout de la création et mise à jour de profils

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -23,5 +24,82 @@ router.get('/me', auth, async (req, res) => {
         res.status(500).send('Erreur serveur ..');
     }
 });
+
+// @route       POST api/profile
+// @desc        Créer ou mettre à jour un profil d'utilisateur
+// @access      Private
+router.post('/', [auth, [
+    check('status', 'Le statut doit être renseigné').not().isEmpty(),
+    check('skills', 'Les compétences doivent être renseignées').not().isEmpty()
+]],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        // Récupération de tous ces champs depuis le corps de la requête
+        const {
+            company,
+            website,
+            location,
+            bio,
+            status,
+            githubusername,
+            skills,
+            youtube,
+            facebook,
+            twitter,
+            instagram,
+            linkedin
+        } = req.body;
+
+        // Construction de l'objet Profile
+        const profileFields = {};
+        profileFields.user = req.user.id;
+        if (company) profileFields.company = company;
+        if (website) profileFields.website = website;
+        if (location) profileFields.location = location;
+        if (bio) profileFields.bio = bio;
+        if (status) profileFields.status = status;
+        if (githubusername) profileFields.githubusername = githubusername;
+        if (skills) {
+            profileFields.skills = skills.split(',').map(skills => skills.trim());
+        }
+
+        // Construction de l'objet Social
+        profileFields.social = {};
+        if (youtube) profileFields.social.youtube = youtube;
+        if (twitter) profileFields.social.twitter = twitter;
+        if (facebook) profileFields.social.facebook = facebook;
+        if (linkedin) profileFields.social.linkedin = linkedin;
+        if (instagram) profileFields.social.instagram = instagram;
+
+        try {
+            let profile = await Profile.findOne({ user: req.user.id });
+
+            if (profile) {
+                // Mise à jour (si un profil existe)
+                profile = await Profile.findOneAndUpdate(
+                    { user: req.user.id },
+                    { $set: profileFields },
+                    { new: true }
+                );
+
+                return res.json(profile);
+            }
+
+            // Création (si aucun profil n'éxiste)
+            profile = new Profile(profileFields);
+
+            await profile.save();
+            res.json(profile);
+
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Erreur serveur');
+        }
+
+    });
 
 module.exports = router;
