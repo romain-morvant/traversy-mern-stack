3f0125f003cb788c063b009744b3923e446bb5ec
Romain Morvant <romainmorvantdev@gmail.com>
2022-02-07 10:18:49 +0100

    Ajout des routes pour l'ajout, l'affichage et la suppression de posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -41,4 +41,67 @@ router.post('/', [auth, [
         }
     });
 
+// @route    GET api/posts
+// @desc     Récupérer tous les posts
+// @access   Private
+router.get('/', auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ date: -1 });
+        res.json(posts);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Erreur serveur');
+    }
+});
+
+// @route       GET api/posts/:id
+// @desc        Récupérer un post via son ID
+// @access      Private
+router.get('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({ msg: 'Article introuvable ..' });
+        }
+
+        res.json(post);
+
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind === 'ObjectID') {
+            return res.status(404).json({ msg: 'Article introuvable ..' });
+        }
+        res.status(500).send('Erreur serveur');
+    }
+});
+
+// @route    DELETE api/posts/:id
+// @desc     Supprimer un post
+// @access   Private
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({ msg: 'Article introuvable ..' });
+        }
+
+        // Vérification de l'utilisateur (Seul le propriétaire du post doit pouvoir le supprimer)
+        if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'Utilisateur non autorisé' });
+        }
+
+        await post.remove();
+
+        res.json({ msg: 'Article supprimé' });
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind === 'ObjectID') {
+            return res.status(404).json({ msg: 'Article introuvable ..' });
+        }
+        res.status(500).send('Erreur serveur');
+    }
+});
+
 module.exports = router;
