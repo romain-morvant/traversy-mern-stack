c93cd436940752b27c4ebf87e0e60155b8b72d73
Romain Morvant <romainmorvantdev@gmail.com>
2022-02-07 10:40:30 +0100

    Ajout des routes pour liker ou disliker un post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -104,4 +104,55 @@ router.delete('/:id', auth, async (req, res) => {
     }
 });
 
+// @route    PUT api/posts/like/:id
+// @desc     Ajout d'un "j'aime' à un post
+// @access   Private
+router.put('/like/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // Vérifier si le like a déja été "aimé" par cet utilisateur
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+            return res.status(400).json({ msg: 'Vous aimez déjà ce post' });
+        }
+
+        post.likes.unshift({ user: req.user.id });
+
+        await post.save();
+
+        res.json(post.likes);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Erreur serveur');
+    }
+});
+
+// @route    PUT api/posts/unlike/:id
+// @desc     Supprimer une mention "j'aime"
+// @access   Private
+router.put('/unlike/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // Vérifier si le like a déja été "aimé" par cet utilisateur
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+            return res.status(400).json({ msg: "Cet article n'a pas de like" });
+        }
+
+        // Récupération de l'index retiré
+        const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+
+        post.likes.splice(removeIndex, 1);
+
+        await post.save();
+
+        res.json(post.likes);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Erreur serveur');
+    }
+});
+
 module.exports = router;
