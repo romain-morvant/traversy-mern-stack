ec67f2d2e6154a4168049fab085a3a4fa1f21a50
Romain Morvant <romainmorvantdev@gmail.com>
2022-02-13 11:33:18 +0100

    Ajout d'une route protégée pour accéder au dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
-import React, {useEffect} from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import store from './store';
-import {loadUser} from './actions/auth';
+import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
@@ -26,16 +28,20 @@ const App = () => {
   return (
     <Provider store={store}>
       <Router>
-        <Navbar/>
-        <Alert/>
+        <Navbar />
+        <Alert />
         <Routes>
-          <Route path='/' element={<Landing/>}/>
-          <Route path='register' element={<Register/>}/>
-          <Route path='login' element={<Login/>}/>
+          <Route path='/' element={<Landing />} />
+          <Route path='register' element={<Register />} />
+          <Route path='login' element={<Login />} />
+          <Route
+            path='dashboard'
+            element={<PrivateRoute component={Dashboard} />}
+          />
         </Routes>
       </Router>
     </Provider>
   );
-}
+};
 
-export default App
+export default App;
